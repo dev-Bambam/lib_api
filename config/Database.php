bcc4764b7e3cb264a9b33ebf6cfc88fa06bed5a5
dev-Bambam <ayogood18@gmail.com>
2024-10-24 12:07:50 +0100

    Lol, can't believe, finding it difficult to write a simple dsn string using interpolation

--- config/Database.php
+++ config/Database.php
@@ -1,1 +1,23 @@
- 
+<?php
+// Bambam, please keep this simple for now ok!
+// creating a database class for database connection
+// fields: host, user, password, database_name
+// methods: static connect()
+// this is actually a static class no instance need to be created
+class Database{
+    private static $host  = 'localhost';
+    private static $user = "root";
+    private static $password = "";
+    private static $db_name = "Library_schema";
+    private static $dsn = "mysql:host=" .self::$host.";dbname=". self::$db_name .";port:3306";
+    static function connect(){
+        try{
+            $pdo = new PDO(self::$dsn, self::$user, self::$password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch (PDOException $e){
+            echo json_encode([
+                "error" =>  $e->getMessage()
+            ]);
+        }
+    }
+}
